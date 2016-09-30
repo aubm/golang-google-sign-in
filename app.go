3ff2a71c81f819bc53f24789cca157ba00c650f6
kendo5731 <aur.baumann@gmail.com>
2016-10-01 01:02:32 +0200

    Changes the codebase in order to deploy on app engine

--- app.go
+++ app.go
@@ -10,23 +10,20 @@ import (
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/mendsley/gojwk"
+	"google.golang.org/appengine/urlfetch"
+	"google.golang.org/appengine"
+	"golang.org/x/net/context"
 )
 
-func main() {
+func init() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", homeHandler)
 	r.HandleFunc("/api", apiHandler)
 	http.Handle("/", r)
-
-	fmt.Println("Application running on port 9999")
-	http.ListenAndServe(":9999", nil)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "./public/index.html")
 }
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+
 	myToken := strings.Replace(r.Header.Get("Authorization"), "Bearer ", "", 1)
 
 	token, err := jwt.Parse(myToken, func(token *jwt.Token) (interface{}, error) {
@@ -34,19 +31,19 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
-		return myLookupKey(token.Header["kid"].(string))
+		return myLookupKey(ctx, token.Header["kid"].(string))
 	})
 
 	fmt.Printf("%v\n", token.Valid)
 	fmt.Printf("%v\n", err)
 }
 
-func myLookupKey(kid string) (interface{}, error) {
+func myLookupKey(ctx context.Context, kid string) (interface{}, error) {
 	//fmt.Printf("Kid : %v\n", kid)
 	var v map[string]interface{}
-	parseJSONFromURL("https://accounts.google.com/.well-known/openid-configuration", &v)
+	parseJSONFromURL(ctx, "https://accounts.google.com/.well-known/openid-configuration", &v)
 	var keys struct{ Keys []gojwk.Key }
-	parseJSONFromURL(v["jwks_uri"].(string), &keys)
+	parseJSONFromURL(ctx, v["jwks_uri"].(string), &keys)
 	for _, key := range keys.Keys {
 		if key.Kid == kid {
 			return key.DecodePublicKey()
@@ -55,8 +52,8 @@ func myLookupKey(kid string) (interface{}, error) {
 	return nil, fmt.Errorf("Key not found")
 }
 
-func parseJSONFromURL(url string, v interface{}) {
-	resp, _ := http.Get(url)
+func parseJSONFromURL(ctx context.Context, url string, v interface{}) {
+	resp, _ := urlfetch.Client(ctx).Get(url)
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(body, v)
